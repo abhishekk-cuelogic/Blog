0d0d7ee6cade902d7fc990f0a0ffbc64368d923f
Abhishek Khutwad <abhishek.khutwad@cuelogic.com>
2018-09-07 19:21:24 +0530

    profile model changed and follower functionality added

--- Server/model/profileModel.js
+++ Server/model/profileModel.js
@@ -2,6 +2,14 @@ import mongoose from 'mongoose';
 mongoose.connect('mongodb://localhost:27017/Blog',{ useNewUrlParser: true });
 const Schema = mongoose.Schema;
 
+const followerSchema = new Schema({
+    userName:String
+});
+
+const followingSchema = new Schema({
+    userName:String
+})
+
 const profileSchema = new Schema({
     userName:String,
     fullName:String,
@@ -9,8 +17,8 @@ const profileSchema = new Schema({
     skills:String,
     passWord:String,
     profileImage:String,
-    followers:Number,
-    following:Number
+    followers:[followerSchema],
+    following:[followingSchema]
 })
 
 const profile = mongoose.model('profile',profileSchema);
--- Server/routes/userProfileRoute.js
+++ Server/routes/userProfileRoute.js
@@ -5,5 +5,9 @@ const router = express.Router();
 
 router.get('/:user',profileController.getProfile);
 router.post('/',profileController.saveProfile);
+router.get('/follower/:user',profileController.getFollowers);
+router.put('/follower/:user',profileController.addFollower);
+router.get('/following/:user',profileController.getFollowings);
+
 
 module.exports = router;
