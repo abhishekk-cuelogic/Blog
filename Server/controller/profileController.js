78b47c26e5ce912772f9a5c7d4d36ca0a67b80cb
Abhishek Khutwad <abhishek.khutwad@cuelogic.com>
2018-09-24 12:38:14 +0530

    multer set

--- Server/controller/profileController.js
+++ Server/controller/profileController.js
@@ -1,9 +1,30 @@
 import profile from '../model/profileModel';
+import multer from 'multer';
 
 
 class profileController {
 
+    storage = multer.diskStorage({
+        destination:'./public',
+        filename:(req,file,cb)=>{
+            cb(null,file.fieldname+'-'+Date.now()+path.extname(file.originalname))
+        }
+    });
+
+    upload = multer ({
+        storage: storage
+    }).single('myImage');
+
+
     saveProfile (req,res) {
+
+        this.upload(req,res, (err) => {
+            if(err) {
+                res.json(err);
+            } else {
+                console.log(req);
+            }
+        })
 
         let userProfile = {
             userName:req.body.userName,
@@ -14,9 +35,9 @@ class profileController {
             profileImage:req.body.profileImage
         }
 
-        let data = new profile(userProfile);
-        data.save();
-        res.send("Profile Saved Successfully");
+        // let data = new profile(userProfile);
+        // data.save();
+        // res.send("Profile Saved Successfully");
     }
 
     getProfile (req,res) {
