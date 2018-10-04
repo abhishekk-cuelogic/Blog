e06d44da6cb1d6380eea42a69826cac252cff21a
Abhishek Khutwad <abhishek.khutwad@cuelogic.com>
2018-10-04 17:32:22 +0530

    useractivity done

---
+++ Server/controller/userActivityController.js
@@ -0,0 +1,40 @@
+import userActivity from '../model/userActivityModel';
+import mongoose from 'mongoose';
+
+class activityController {
+
+    saveActivity (req,res) {
+        userActivity.findOne({userName:req.body.userName},(err,activity) => {
+            if(err) {
+                res.json(err);
+            } else {
+                if(activity === null) {
+                    let a ={
+                        userName : req.body.userName,
+                        useractivity : [{
+                            activity:req.body.activity
+                        }]
+                    }
+                    let data = new userActivity(a);
+                    data.save();
+                } else {
+                    activity.useractivity.push({activity:req.body.activity});
+                    activity.save();
+                }
+            }
+            res.json('done');
+        })
+    }
+
+    getActivity (req,res) {
+        userActivity.findOne({userName:req.params.userName}, (err,doc)=> {
+            if(err) {
+                res.json(err);
+            } else {
+                res.json(doc);
+            }
+        })
+    }
+}
+
+export default new activityController();
